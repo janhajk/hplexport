9f12e7ec008ec5e6cda577b88d4ea35ee5c32834
Jan <janhajk@gmail.com>
2017-01-10 09:23:36 +0000

    path

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ var createPath = function(node) {
    if (node.type==='baujournal' || node.type==='ausmasskontrolle') {
       phase = 'Realisierung';
    }
-   elseif (nodey.type === 'projektjournal') {
+   else if (nodey.type === 'projektjournal') {
       phase = 'Projektierung';
    }
    else {
