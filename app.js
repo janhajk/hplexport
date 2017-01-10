39c4f2f6dadc1f0f155fd0ee443c85b0b2da6658
Jan <janhajk@gmail.com>
2017-01-10 09:01:45 +0000

    path

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ var getNodes = function(callback) {
                         nodesVid[i].terms['Abschnitt'] = termOrder(nodesVid[i].terms['Abschnitt']);
                         nodesVid[i].datum = cleanupDate(nodesVid[i].datum);
                      }
-                     console.log(nodesVid);
+                     console.log(dump(nodesVid));
                      callback(null, nodesVid);
                   }
                });
@@ -140,7 +140,10 @@ var createPath = function(node) {
    for(let i in node.terms.Abschnitt) {
       phase.push(node.terms.Abschnitt[i]);
    }
-   return '/' + [node.projektphase, phase.join('/'), node.datum + '_' + node.title].join('/');
+   var datum = node.datum===null?'':node.datum+'_';
+   var pfad = '/' + [node.projektphase, phase.join('/'), datum + node.title].join('/');
+   pfad = pfad.replace(/<|>|\?|"|:|\||\\|\/|\*/g,'');
+   return pfad;
 };
 
 getNodes(function(err, nodes){
@@ -189,6 +192,40 @@ var fileExport  = function(filetree) {
 getFileTree(function(err, rows){
    export(rows);
 });*/
+
+
+/**
+ * Function : dump()
+ * Arguments: The data - array,hash(associative array),object
+ *    The level - OPTIONAL
+ * Returns  : The textual representation of the array.
+ * This function was inspired by the print_r function of PHP.
+ * This will accept some data as the argument and return a
+ * text that will be a more readable version of the
+ * array/hash/object that is given.
+ * Docs: http://www.openjs.com/scripts/others/dump_function_php_print_r.php
+ */
+var dump = function(arr, level) {
+   var dumped_text = "";
+   if(!level) level = 0;
+   //The padding given at the beginning of the line.
+   var level_padding = "";
+   for(var j = 0; j < level + 1; j++) level_padding += "    ";
+   if(typeof(arr) == 'object') { //Array/Hashes/Objects 
+      for(var item in arr) {
+         var value = arr[item];
+         if(typeof(value) == 'object') { //If it is an array,
+            dumped_text += level_padding + "'" + item + "' ...\n";
+            dumped_text += dump(value, level + 1);
+         } else {
+            dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+         }
+      }
+   } else { //Stings/Chars/Numbers etc.
+      dumped_text = "===>" + arr + "<===(" + typeof(arr) + ")";
+   }
+   return dumped_text;
+}
 
 
 
